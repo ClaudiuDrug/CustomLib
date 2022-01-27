c4a1b49feb3f184edfa845ecf241880928cc8cf2
Claudiu DRUG <claudiu.drug@outlook.com>
2022-01-27 21:11:14 +0200

    [27.01.2022] - Added references to `CfgSingleton` and `LogSingleton` in `__init__.py`

--- customlib/__init__.py
+++ customlib/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: UTF-8 -*-
 
+from os.path import join
+
 from .cfgparser import CfgSingleton
 from .constants import DEFAULTS, BACKUP
+from .logging import LogSingleton
 from .utils import get_path
-from os.path import join
 
 DEFAULTS.update({"directory": get_path()})
 CONFIG: str = join(DEFAULTS.get("directory"), "config", "config.ini")
@@ -11,3 +13,5 @@ CONFIG: str = join(DEFAULTS.get("directory"), "config", "config.ini")
 cfg = CfgSingleton()
 cfg.set_defaults(**DEFAULTS)
 cfg.read_dict(dictionary=BACKUP, source="<backup>")
+
+log = LogSingleton()
